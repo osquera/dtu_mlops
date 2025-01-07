fbad0a71496ed4fb2d61fd9984a14d1400c95177
Anton M. Storgaard <antonmosquerastorgaard@gmail.com>
2025-01-07 14:20:44 +0100

    updated tasks and requirements

--- my_project/tasks.py
+++ my_project/tasks.py
@@ -19,6 +19,22 @@ def git(ctx, message):
     ctx.run(f"git commit -m '{message}'")
     ctx.run(f"git push")
 
+@task
+def dvc(ctx, folder="data", message="Add new data"):
+    ctx.run(f"dvc add {folder}")
+    ctx.run(f"git add {folder}.dvc .gitignore")
+    ctx.run(f"git commit -m '{message}'")
+    ctx.run(f"git push")
+    ctx.run(f"dvc push")
+
+@task
+def pull_data(ctx):
+    ctx.run("dvc pull")
+
+@task(pull_data)
+def train(ctx):
+    ctx.run("my_cli train")
+
 # Setup commands
 @task
 def create_environment(ctx: Context) -> None:
