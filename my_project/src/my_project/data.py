3fd4fdc8a38c6c11e8b1ab11c4a328fa67a9635d
Anton M. Storgaard <antonmosquerastorgaard@gmail.com>
2025-01-10 00:06:10 +0100

    small fix

--- my_project/src/my_project/data.py
+++ my_project/src/my_project/data.py
@@ -54,10 +54,10 @@ def preprocess_data(raw_dir: str, processed_dir: str) -> None:
 
 def corrupt_mnist() -> tuple[torch.utils.data.Dataset, torch.utils.data.Dataset]:
     """Return train and test datasets for corrupt MNIST."""
-    train_images = torch.load("data/processed/train_images.pt")
-    train_target = torch.load("data/processed/train_target.pt")
-    test_images = torch.load("data/processed/test_images.pt")
-    test_target = torch.load("data/processed/test_target.pt")
+    train_images = torch.load("data/processed/train_images.pt", weights_only=False)
+    train_target = torch.load("data/processed/train_target.pt", weights_only=False)
+    test_images = torch.load("data/processed/test_images.pt", weights_only=False)
+    test_target = torch.load("data/processed/test_target.pt", weights_only=False)
 
     train_set = torch.utils.data.TensorDataset(train_images, train_target)
     test_set = torch.utils.data.TensorDataset(test_images, test_target)
