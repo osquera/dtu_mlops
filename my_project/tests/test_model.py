4f1257c0c87188925d3756e0580188dccc898eae
GJOEIUQ <anton.storgaard@man-es.com>
2025-01-13 14:07:19 +0100

    Added another test

---
+++ my_project/tests/test_model.py
@@ -0,0 +1,9 @@
+import torch
+from my_project.model import MyAwesomeModel
+
+
+def test_model():
+    model = MyAwesomeModel()
+    x = torch.randn(1, 1, 28, 28)
+    y = model(x)
+    assert y.shape == (1, 10)
