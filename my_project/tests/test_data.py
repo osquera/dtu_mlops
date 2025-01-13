92d76c2aef02de7badfbd91c7bbe010f2220852b
GJOEIUQ <anton.storgaard@man-es.com>
2025-01-13 14:27:41 +0100

    updated tests

--- my_project/tests/test_data.py
+++ my_project/tests/test_data.py
@@ -1,18 +1,25 @@
 from torch.utils.data import Dataset
 import torch
+import pytest
 from my_project.data import MyDataset
 from my_project.data import corrupt_mnist
+import os
+
+_TEST_ROOT = os.path.dirname(__file__)  # root of test folder
+_PROJECT_ROOT = os.path.dirname(_TEST_ROOT)  # root of project
+_PATH_DATA = os.path.join(_PROJECT_ROOT, "data")  # root of data
 
 
+@pytest.mark.skipif(not os.path.exists(_PATH_DATA), reason="Data not found")
 def test_data():
     train, test = corrupt_mnist()
-    assert len(train) == 30000
-    assert len(test) == 5000
+    assert len(train) == 30000, "Train dataset did not have the correct number of samples"
+    assert len(test) == 5000, "Test dataset did not have the correct number of samples"
     for dataset in [train, test]:
         for x, y in dataset:
             assert x.shape == (1, 28, 28)
             assert y in range(10)
     train_targets = torch.unique(train.tensors[1])
-    assert (train_targets == torch.arange(0, 10)).all()
+    assert (train_targets == torch.arange(0, 10)).all(), "Train targets were not correct"
     test_targets = torch.unique(test.tensors[1])
-    assert (test_targets == torch.arange(0, 10)).all()
+    assert (test_targets == torch.arange(0, 10)).all(), "Test targets were not correct"
--- my_project/tests/test_model.py
+++ my_project/tests/test_model.py
@@ -1,4 +1,5 @@
 import torch
+import pytest
 from my_project.model import MyAwesomeModel
 
 
@@ -6,4 +7,14 @@ def test_model():
     model = MyAwesomeModel()
     x = torch.randn(1, 1, 28, 28)
     y = model(x)
-    assert y.shape == (1, 10)
+    assert y.shape == (1, 10), f"Expected output shape (1, 10), but got {y.shape}"
+
+
+@pytest.mark.parametrize("batch_size", [32, 64])
+def test_model(batch_size: int) -> None:
+    model = MyAwesomeModel()
+    x = torch.randn(batch_size, 1, 28, 28)
+    y = model(x)
+    assert y.shape == (batch_size, 10)
+
+# coverage run --omit="tests/*" -m pytest tests/
