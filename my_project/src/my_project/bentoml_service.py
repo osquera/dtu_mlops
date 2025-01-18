e4366d08a2a69fd723ed181394863678765ff5bc
Anton M. Storgaard <antonmosquerastorgaard@gmail.com>
2025-01-18 17:20:26 +0100

    addded bentoml

---
+++ my_project/src/my_project/bentoml_service.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+import os
+
+import bentoml
+import numpy as np
+from onnxruntime import InferenceSession
+
+
+@bentoml.service
+class ImageClassifierService:
+    """Image classifier service using ONNX model."""
+
+    def __init__(self) -> None:
+        self.model = InferenceSession(r"C:\DTU_HCAI\1_sem\mlops\dtu_mlops\my_project\models\model.onnx")
+
+    @bentoml.api
+    def predict(self, image: np.ndarray) -> np.ndarray:
+        """Predict the class of the input image."""
+        output = self.model.run(None, {"input": image.astype(np.float32)})
+        return output[0]
